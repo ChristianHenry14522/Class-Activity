91c3eaadb0ee9706917e05e1dd7c9c4e8ecb266b
itsch <tuq14522@temple.edu>
2025-09-09 12:25:10 -0400

    Class Activity

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,14 +1,30 @@
 package edu.temple.classactivity
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.messageTextView)
+        val textView = findViewById<EditText>(R.id.editTextText)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            if (editText.text.isNotEmpty()) {
+                "Hello, " + editText.text + "! Welcome."
+            }
+
+        } else {
+            "Please enter your mame."
+
+
+
+        }
+
+
     }
 }
